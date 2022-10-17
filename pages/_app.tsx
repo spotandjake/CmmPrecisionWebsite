de8d4e7e93364b90d79d4baa2f0b57e72f1f644f
Spotandjake <spotandjake@hotmail.com>
2022-10-17 19:27:06 -0400

    Work On Getting Google Analyitics To Work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics trackPageViews strategy="lazyOnload" />
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
   );
