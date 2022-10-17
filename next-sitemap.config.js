01f491b46a710e92e7667ef2a1a73aba30eb9409
Spotandjake <spotandjake@hotmail.com>
2022-10-17 18:41:59 -0400

    Use A http Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://cmmprecision.ca/',
+  siteUrl: 'http://cmmprecision.ca/',
   generateRobotsTxt: true, // (optional)
   // ...other options
-}
+};
