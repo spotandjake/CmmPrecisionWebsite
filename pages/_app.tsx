d5b47193436890660eb452ee826a593c1fa59dda
Spotandjake <spotandjake@hotmail.com>
2022-10-17 18:50:39 -0400

    Enable Google Analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 // Styles
 import '../_static/Style/Theme/Dark.scss';
 import '../_static/Style/Global.scss';
+// Imports
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 // Types
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
