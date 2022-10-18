16371bd34d312c6faf49e8340f6cade01912ffb0
Spotandjake <spotandjake@hotmail.com>
2022-10-17 21:12:07 -0400

    Add Ms Clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
+      <script
+        id="msClarity"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "e4pgy55aul");
+        `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Home = () => {
               <p>
                 With over 30 years of experience in the measurement field, we have the ability to provide a diverse range of quality services to our clients. Cmm Precision offers a wide array of quality services to many clients in the automotive, aerospace, medical, and many other industries.
               </p>
-              <Link href={Navigation.Pages['About']} passHref={true}><button>Read More</button></Link>
+              <Link href={Navigation.Pages['About Us']} passHref={true}><button>Read More</button></Link>
             </div>
           </div>
         </div>
