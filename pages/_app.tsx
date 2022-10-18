32eefc8148f5cff227a3f0ded7167dcab43221a2
Spotandjake <spotandjake@hotmail.com>
2022-10-17 21:12:58 -0400

    Change Key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "e4pgy55aul");
+          })(window, document, "clarity", "script", "e4pkw0pjg0");
         `,
         }}
       />
