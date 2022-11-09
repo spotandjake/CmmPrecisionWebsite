9f5c3cec69e73c0aad76b8d017fa638116c2ef28
Spotandjake <spotandjake@hotmail.com>
2022-11-09 17:00:06 -0500

    chore: Clean up some img settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 // Styles
 import '../_static/Style/Theme/Dark.scss';
 import '../_static/Style/Global.scss';
-// Imports
-import Script from 'next/script';
 // Types
 import type { AppProps } from 'next/app';
 
@@ -29,24 +27,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
       />
       {/* Microsoft Clarity */}
-      {/* <script
-        id="msClarity"
-        defer
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){
-              (c[a].q=c[a].q||[]).push(arguments)
-            };
-            t=l.createElement(r);
-            t.async=1;
-            t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-            y=l.getElementsByTagName(r)[0];
-            y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "e4pkw0pjg0");
-        `,
-        }}
-      /> */}
       <script
         id="msClarity"
         dangerouslySetInnerHTML={{
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ import { Navigation } from '../_static/Navigation';
 const Home = () => {
   return (
     <section>
-      <MetaData PageTitle='Home' />
+      <MetaData PageTitle="Home" />
       {/* Header */}
-      <Header Active='Home' />
+      <Header Active="Home" />
       {/* Hero */}
       <Hero>
         <h2>Cmm Precision</h2>
@@ -33,15 +33,28 @@ const Home = () => {
               <Image
                 src={AboutImage}
                 alt="Image Of Cmm With Part"
-                width={650}
+                width={750}
                 height={375}
+                sizes="(max-height: 375px) 100vh, (max-height: 375px) 0vh"
+                layout="responsive"
               />
             </div>
             <div className={styles.gridText}>
               <p>
-                With over 30 years of experience in the measurement field, we have the ability to provide a diverse range of quality services to our clients such as measurement, rework, consulting and an assortment of other services. Cmm Precision offers a wide array of quality services to many clients in the automotive, aerospace, medical, and many other industries.
+                With over 30 years of experience in the measurement field, we
+                have the ability to provide a diverse range of quality services
+                to our clients such as measurement, rework, consulting and an
+                assortment of other services. Cmm Precision offers a wide array
+                of quality services to many clients in the automotive,
+                aerospace, medical, and many other industries.
               </p>
-              <Link href={Navigation.Pages['About Us']} passHref={true} prefetch={false}><button>More About Us</button></Link>
+              <Link
+                href={Navigation.Pages['About Us']}
+                passHref={true}
+                prefetch={false}
+              >
+                <button>More About Us</button>
+              </Link>
             </div>
           </div>
         </div>
@@ -51,18 +64,24 @@ const Home = () => {
           <div className={styles.gridCol2}>
             <div>
               <p className={styles.gridText}>
-                Cmm Precision has been serving clients for over 30 years with high quality precision measurements and services. Cmm Precision allows you to focus on your business while we focus on the quality of your products.
+                Cmm Precision has been serving clients for over 30 years with
+                high quality precision measurements and services. Cmm Precision
+                allows you to focus on your business while we focus on the
+                quality of your products.
               </p>
               <p>
-                Having served jobs for multiple large multinational corporations and small local businesses Cmm Precision is perfect for any quality services you may need.
+                Having served jobs for multiple large multinational corporations
+                and small local businesses Cmm Precision is perfect for any
+                quality services you may need.
               </p>
             </div>
             <div className={styles.pictureBox}>
               <Image
                 src={ExperienceImage}
                 alt="Image Of Cmm With Part"
-                width={650}
+                width={750}
                 height={375}
+                sizes="(max-height: 375px) 100vh, (max-height: 375px) 0vh"
               />
             </div>
           </div>
@@ -72,24 +91,48 @@ const Home = () => {
           <h3>Services</h3>
           <div className={styles.GridCards3}>
             {/* TODO: Replace Image */}
-            <ImageCard ImagePath={'/Images/Hero.jpg'} OverlayText={'Fixture Certification'} />
-            <ImageCard ImagePath={'/Images/About.jpg'} OverlayText={'Part Inspection'} />
+            <ImageCard
+              ImagePath={'/Images/Hero.jpg'}
+              OverlayText={'Fixture Certification'}
+            />
+            <ImageCard
+              ImagePath={'/Images/About.jpg'}
+              OverlayText={'Part Inspection'}
+            />
             {/* TODO: Replace Image */}
-            <ImageCard ImagePath={'/Images/Hero.jpg'} OverlayText={'Consultation'} />
+            <ImageCard
+              ImagePath={'/Images/Hero.jpg'}
+              OverlayText={'Consultation'}
+            />
             {/* TODO: Replace Image */}
-            <ImageCard ImagePath={'/Images/Hero.jpg'} OverlayText={'On Site Inspection'} />
+            <ImageCard
+              ImagePath={'/Images/Hero.jpg'}
+              OverlayText={'On Site Inspection'}
+            />
             {/* TODO: Replace Image */}
-            <ImageCard ImagePath={'/Images/Hero.jpg'} OverlayText={'Reverse Engineering'} />
+            <ImageCard
+              ImagePath={'/Images/Hero.jpg'}
+              OverlayText={'Reverse Engineering'}
+            />
             {/* TODO: Replace Image */}
-            <ImageCard ImagePath={'/Images/Hero.jpg'} OverlayText={'Part Rework & Sorting'} />
+            <ImageCard
+              ImagePath={'/Images/Hero.jpg'}
+              OverlayText={'Part Rework & Sorting'}
+            />
           </div>
         </div>
         {/* Equipment */}
         <div className={styles.Article}>
           <h3>Equipment</h3>
           <div className={styles.GridCards2}>
-            <ImageCard ImagePath={'/Images/cmm.jpg'} OverlayText={'Brown & Sharp One CMM'} />
-            <ImageCard ImagePath={'/Images/romorArm.jpg'} OverlayText={'Infinity 2.0 Romor Arm'} />
+            <ImageCard
+              ImagePath={'/Images/cmm.jpg'}
+              OverlayText={'Brown & Sharp One CMM'}
+            />
+            <ImageCard
+              ImagePath={'/Images/romorArm.jpg'}
+              OverlayText={'Infinity 2.0 Romor Arm'}
+            />
           </div>
         </div>
       </main>
