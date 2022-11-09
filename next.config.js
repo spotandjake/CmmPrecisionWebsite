980d3405f84c7977ca2a611f514c5affc3047cbb
Spotandjake <spotandjake@hotmail.com>
2022-11-09 15:48:22 -0500

    feat: Test Using Preact

--- next.config.js
+++ next.config.js
@@ -25,4 +25,14 @@ module.exports = withBundleAnalyzer({
     locales: ['en'],
     defaultLocale: 'en',
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
 });
